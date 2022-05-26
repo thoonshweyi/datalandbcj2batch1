ae3fecbf0a023850b9407a8b1bacc659aae5dd23
Thoon Shwe Yi Kyaw <thoonlay779@gmail.com>
2022-05-26 20:30:12 +0630

    26May22

---
+++ landingpage/js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    // START SHOWCASE SLIDER SECTION
+    $('.carousel').carousel({
+        // carousel speed
+        interval:6000,
+
+        // default
+        pause:'hover' 
+    });
+    // END SHOWCASE SLIDER SECTION
+})
---
+++ landingpagepractice/js/app.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    
+    $('.carousel').carousel({
+        interval : 6000,
+        pause : 'hover'
+    });
+
+})
